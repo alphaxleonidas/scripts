7be73ec19d415b5327d167c3f7e251f824e9d7cf
alphaxleonidas <alphaxleonidas@gmail.com>
2026-06-25 04:06:31 +0500

    Update URLs for script updates and downloads

--- bypassfreedlink.user.js
+++ bypassfreedlink.user.js
@@ -15,8 +15,8 @@
 // @match       https://fredl.*/*
 // @version     1.4.3
 // @license     MIT
-// @updateURL    https://github.com/alphaxleonidas/redirect-scripts/raw/refs/heads/main/medium-to-readmedium.user.js
-// @downloadURL  https://github.com/alphaxleonidas/redirect-scripts/raw/refs/heads/main/medium-to-readmedium.user.js
+// @updateURL    https://github.com/alphaxleonidas/Scripts/raw/refs/heads/main/bypassfreedlink.user.js
+// @downloadURL  https://github.com/alphaxleonidas/Scripts/raw/refs/heads/main/bypassfreedlink.user.js
 // @author      Luka Mamukashvili <mamukashvili.luka@usltd.ge>
 // ==/UserScript==
 
