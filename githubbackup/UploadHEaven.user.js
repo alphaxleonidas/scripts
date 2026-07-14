72787c8926419cf3fb446152dfce027ed124f125
alphaxleonidas <alphaxleonidas@gmail.com>
2026-07-14 23:22:08 +0500

    Update UploadHEaven.user.js

--- githubbackup/UploadHEaven.user.js
+++ githubbackup/UploadHEaven.user.js
@@ -5,8 +5,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.4
 // @description  Bypasses Uploadhaven time restriction.
-// @updateURL    https://github.com/alphaxleonidas/Scripts/raw/refs/heads/main/UploadHEaven.user.js
-// @downloadURL  https://github.com/alphaxleonidas/Scripts/raw/refs/heads/main/UploadHEaven.user.js
+// @updateURL    https://github.com/alphaxleonidas/Scripts/raw/refs/heads/main/githubbackup/UploadHEaven.user.js
+// @downloadURL  https://github.com/alphaxleonidas/Scripts/raw/refs/heads/main/githubbackup/UploadHEaven.user.js
 // @author       crapbass#8715
 // @match      https://uploadhaven.com/download/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=uploadhaven.com
