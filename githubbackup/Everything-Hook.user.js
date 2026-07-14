fa6e11fef89924854b1558edafcb82a00926235f
alphaxleonidas <alphaxleonidas@gmail.com>
2026-07-14 23:21:49 +0500

    Update Everything-Hook.user.js

--- githubbackup/Everything-Hook.user.js
+++ githubbackup/Everything-Hook.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Everything-Hook
 // @namespace    https://gitee.com/HGJing/everthing-hook/
-// @updateURL    https://github.com/alphaxleonidas/Scripts/raw/refs/heads/main/Everything-Hook.user.js
+// @updateURL    https://github.com/alphaxleonidas/Scripts/raw/refs/heads/main/githubbackup/Everything-Hook.user.js
 // @version      0.5.9056
 // @include      *
 // @description  it can hook everything
